c4cf62c214dd8316fbb911cd1d7247b11975f403
CheeseCoffee <akbarsky93@mail.ru>
2020-07-07 23:51:45 +0300

    begin

---
+++ MaxElementIf/cmake-build-debug/CMakeFiles/MaxElementIf.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MaxElementIf.dir/main.cpp.o"
+  "MaxElementIf.exe"
+  "MaxElementIf.pdb"
+  "libMaxElementIf.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MaxElementIf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
