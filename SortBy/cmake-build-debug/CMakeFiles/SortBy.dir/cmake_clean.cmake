4b39f5593bc3d8013792e5b9e2c9aa21b5b07f57
CheeseCoffee <akbarsky93@mail.ru>
2020-07-10 21:27:34 +0300

    Sortby done

---
+++ SortBy/cmake-build-debug/CMakeFiles/SortBy.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SortBy.dir/sort_by.cpp.o"
+  "SortBy.exe"
+  "SortBy.pdb"
+  "libSortBy.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SortBy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
