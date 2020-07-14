184e7c501da398457f01af05fe3713da82bbc57f
CheeseCoffee <akbarsky93@mail.ru>
2020-07-14 21:48:47 +0300

    table

---
+++ Table/cmake-build-debug/CMakeFiles/Table.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Table.dir/table.cpp.o"
+  "Table.exe"
+  "Table.pdb"
+  "libTable.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Table.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
