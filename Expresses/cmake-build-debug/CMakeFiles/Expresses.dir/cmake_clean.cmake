5a43f2f5f5d21cba8f4be0757654850836103120
CheeseCoffee <akbarsky93@mail.ru>
2020-07-29 18:19:26 +0300

    express

---
+++ Expresses/cmake-build-debug/CMakeFiles/Expresses.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Expresses.dir/main.cpp.o"
+  "Expresses"
+  "Expresses.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Expresses.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
